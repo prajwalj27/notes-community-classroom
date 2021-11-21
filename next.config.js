afec31f0ca8bd1c2738c5e66a44d35315d6b4ff0
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2021-11-21 23:12:01 +0530

    Add repo name

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+// Replace `nextjs-github-pages` with your Github repo project name.
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : ''
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/notes-community-classroom/' : '',
 }
