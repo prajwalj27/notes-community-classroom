de519390d805378d800c05ff4614904e45ffef08
Prajwal Jaiswal <prajwal.jaiswal@hotmail.com>
2021-11-22 00:25:11 +0530

    Add favicon file

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import Header from '../components/Header'
 import '../styles/globals.css'
 
+const prefix = '/notes-community-classroom'
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <link rel="shortcut icon" href={prefix+'/favicon.ico'} />
       <Header />
       <main className='container'>
         <Component {...pageProps} />
